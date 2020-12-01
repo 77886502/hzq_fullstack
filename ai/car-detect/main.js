c6b98484213096767c5a803ebd76dccb7d763562
77886502 <1604125861@qq.com>
2020-12-01 21:45:51 +0800

    ai

--- ai/car-detect/main.js
+++ ai/car-detect/main.js
@@ -5,13 +5,14 @@ const AipocrClient = require('baidu-aip-sdk').ocr;
 //内置模块 fileSystem
 // const fs = require('fs');
 // 1. 读入文件 I/O操作 从硬盘读入内存中，
-
+const fs = require('fs');
 const APP_ID="23078069";
 const APP_KEY= "nowp4yXKY3Ie5pbXl0WQzYU6";
 const SECRET_KEY="R9UyNGinSTvzqMY0CexZGO5L3hDjwng4";
+
 const client = new AipocrClient(APP_ID,APP_KEY,SECRET_KEY);
 const image = fs.readFileSync("car_id.jpg").toString("base64"); 
-const fs = require('fs');
+
 const options={};
 options["multi_detect"]="true"
 

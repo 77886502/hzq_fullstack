d47fd24bbb0f49bac44fd82c27ac67e46608cb0c
77886502 <1604125861@qq.com>
2020-12-04 21:39:04 +0800

    12/04

---
+++ node/cherrio/index.js
@@ -0,0 +1,14 @@
+let fetch = require("node-fetch"); //内置
+let cheerio = require('cheerio');
+let url = 'http://movie.douban.com/top250';
+const main = async() => { // async 关键字
+   fetch(url)
+      
+        .then(res => res.text())
+        .then(body => {
+           let $ = cheerio.load(body);
+           let movieNodes = $('content.item');
+           console.log(movieNodes)
+        });
+}
+main();

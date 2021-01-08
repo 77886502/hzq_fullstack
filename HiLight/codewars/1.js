7bba3d2ef33126b6419af620eb89febb5d2f84af
77886502 <1604125861@qq.com>
2021-01-08 18:31:28 +0800

    1.8

---
+++ HiLight/codewars/1.js
@@ -0,0 +1,28 @@
+// Does my number look big in this? 确认一个数字是不是 “自恋数”
+/*
+    数字拆分 123 ---> {3,2,1}
+*/
+function narcissistic(value) {
+    if(value <= 10)
+      return true;
+    var number = new Array();
+    var sum = 0;
+    var target = value;
+    var i=0;
+    while(target!=0)
+    {
+        number[i]=target%10;
+        target=Math.floor(target/=10);
+        i++;
+    }
+    for(let i=0; i<number.length;i++)
+    {   
+       
+       sum=sum+Math.pow(number[i],number.length);
+    }
+    console.log(sum);
+    if(sum == value)
+      return true;
+    else
+      return false;
+}

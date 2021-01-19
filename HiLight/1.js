4bb995b39c40c6357cf649fc7680fb1f0df1e85d
77886502 <1604125861@qq.com>
2021-01-19 19:08:08 +0800

    2021.1.19

--- HiLight/1.js
+++ HiLight/1.js
@@ -1,21 +1,8 @@
-// 1. 数组中的成员类型相同，顺序可以不同。例如[1, true] 与 [false, 2]是相似的。
-// 2. 数组的长度一致。
-// 3. 类型的判断范围，需要区分:String, Boolean, Number, undefined, null, 函数，日期, window.
-function arraysSimilar(arr1, arr2){
-  if(arr1.length!=arr2.length)
-    return false;
-  for(let i=0;i<arr1.length;i++)
-  {
-    arr1[i]=Object.prototype.toString.call(arr1[i]);
-    arr2[i]=Object.prototype.toString.call(arr2[i]);
-  }
-  arr1.sort();
-  arr2.sort();
-  for(let i=0;i<arr1.length;i++)
-  {
-    if(arr1[i]!=arr2[i])
-      return false;
-  }
-  return true;
+// 2^n: 1 2 4 8...
+// 3^n: 3 9 27...
+// 5^n: 5 25 125... 
+// %2 %3 %5
+
+function hamming (n) {
+  
 }
-console.log(arraysSimilar(["abc"],["cba"]));

27112540065b03a8b84eeb65f3c01e4c97a81243
77886502 <1604125861@qq.com>
2021-02-28 21:25:10 +0800

    2.28

--- react/comment-app/src/index.js
+++ react/comment-app/src/index.js
@@ -1,19 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import CommentApp from './CommentApp';
-import reportWebVitals from './reportWebVitals';
-import CommentForm from './CommentForm';
-import CommentList from './CommentList';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CommentApp from './CommentApp'
+import './index.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <CommentApp />
-  </React.StrictMode>,
+  <CommentApp />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ react/react-hooks/src/app.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { HelloComponent } from './hello';
+import { NameEditComponent } from './nameEdit';
+
+// class App extends Component {
+//   constructor() {
+//     this.state = {
+
+//     }
+//   }
+//   render() {
+//     return (
+
+//     )
+//   }
+// }
+
+// StatelessComponent
+export const App = () => {
+  // React hooks name 状态的名字， setName 修改状态用setName 
+//   class bind.this 是最高级别
+  const [name, setName] = React.useState('defaultUserName') // state 
+  // react hooks 
+  // 函数式组件可以借助一些魔法函数来实现状态 
+//   const setUsernameState = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     setName(event.target.value)
+//   }
+    setTimeout(()=>{
+        setName('aaaa')
+    },3000)
+  return (
+    <>
+        <HelloComponent userName={name} />
+        {/* <NameEditComponent userName={name} onChange={setUsernameState}/>    */}
+    </>
+  )
+}

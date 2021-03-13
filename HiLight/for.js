024de96b546a2767e3eedd5bed25027965abdb5c
77886502 <1604125861@qq.com>
2021-03-14 00:03:09 +0800

    3.14

--- HiLight/for.js
+++ HiLight/for.js
@@ -1,38 +1,13 @@
-// 0 => 2 1 => 3 2 => 4 3 => 5 4 => 0 5 => 1 
-var rotate = function(nums, k) {
-    if(k%nums.length!=0)
+
+
+function IsCommon(a,b){
+
+    while (a%b != 0)
     {
-        if(nums.length%k==0)
-        {
-            
-            for(let i=0;i<k;i++)
-            {
-                let length = 0,item=nums[i];
-                while(length<nums.length)
-                {
-                    nums[(i+k)%nums.length] = nums[(i+k)%nums.length] + item;
-                    item = nums[(i+k)%nums.length] - item;
-                    nums[(i+k)%nums.length] = nums[(i+k)%nums.length] - item;
-                    i = (i+k)%nums.length;
-                    length=length+k;
-                }
-            }
-        }
-        else
-        {
-            let length =0, i=0, item = nums[0];
-            while(length<nums.length)
-            {
-                console.log(i);
-                nums[(i+k)%nums.length] = nums[(i+k)%nums.length] + item;
-                item = nums[(i+k)%nums.length] - item;
-                nums[(i+k)%nums.length] = nums[(i+k)%nums.length] - item;
-                i = (i+k)%nums.length;
-                length++;
-            }
-        }
+        tmp=a%b;//取余
+        a = b;//交换a，b可避免a比b小
+        b = tmp;
     }
-};
-let arr = [1,2,3,4,5,6];
-rotate(arr,4);
-console.log(arr);
+    return b;
+}
+console.log(IsCommon(8,9));

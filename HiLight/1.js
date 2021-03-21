941d9f1a56231c8daa8e89e807477eff2e509d25
77886502 <1604125861@qq.com>
2021-03-21 10:42:37 +0800

    3.21

--- HiLight/1.js
+++ HiLight/1.js
@@ -1,21 +1,6 @@
-var moveZeroes = function(nums) {
-    let i = 0;
-    for(let j=0;j<nums.length-1;j++)
-    {
-        if(nums[j]==0)
-        {
-            for(i=j+1;i<nums.length;i++)
-            {
-                if(nums[i]!=0)
-                {
-                    [nums[j],nums[i]] = [nums[i],nums[j]];
-                    break;
-                }
-            }
-      
-        }
-        if(nums[j] == 0)
-            break;
-    }
-};
-console.log(moveZeroes([0,1,0,3,12]));
+let arr = [1,1,2,2,3,3,4,5,5],res=0 ;
+for(let i=0;i<arr.length;i++)
+{
+    if(arr.IndexOf(arr[i])==arr.lastIndexOf(arr[i]))
+        return arr[i];
+}

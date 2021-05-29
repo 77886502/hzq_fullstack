4ea483fb1547cc73877b92f8215847279843feac
Light1013 <1604125861@qq.com>
2021-05-29 22:10:54 +0800

    5.29

--- EveryDay/5.28/1.js
+++ EveryDay/5.28/1.js
@@ -1,13 +1,6 @@
-var faltten = function(arr){
-    var Arr = [];
-    for(var i=0; i<arr.length;i++)
-    {
-        if(Array.isArray(arr[i]))
-            Arr = Arr.concat(faltten(arr[i]))
-        else
-            Arr.push(arr[i])
-    }
-    return Arr;
+var Flatten = function(arr){
+    var str = '['+JSON.stringify(arr).replace(/[\[|\]]/g,'')+']';
+    return JSON.parse(str);
 }
-var arr = [1,[2],[[3],4,[5,[6]]]];
-console.log(faltten(arr));
+var arr = [1,[2,3,[4],[5]],6,[[[[7]],8],9]];
+console.log(Flatten(arr)); //[1,2,3,4,5,6,7,8,9]
